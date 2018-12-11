8313c00cf2d4b4021fc8a40fc6336c14e8586bd1
yanlele <331393627@qq.com>
2018-12-11 21:08:47 +0800

    [+]添加一个 redux 单独使用的示例

--- src/redux/reduceres.js
+++ src/redux/reduceres.js
@@ -2,3 +2,11 @@
  * create by yanle
  * create time 2018/12/11 下午4:29
  */
+
+import counter from './reducers/counter';
+
+export default function combineReducers(state={}, action) {
+    return {
+        counter: counter(state.counter, action)
+    }
+}
