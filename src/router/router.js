e29bd65103d2d60fc2c1d646d12fe8adabffaa46
yanlele <331393627@qq.com>
2018-12-12 11:35:24 +0800

    [+]添加使用示例

--- src/router/router.js
+++ src/router/router.js
@@ -9,6 +9,7 @@ import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import Home from 'pages/Home/Home';
 import Page1 from 'pages/Page1/Page1';
 import Counter from '../pages/Counter/Counter';
+import UserInfo from "../pages/UserInfo/UserInfo";
 
 const getRouter = () => (
     <Router>
@@ -23,12 +24,16 @@ const getRouter = () => (
                 <li>
                     <Link to='/counter'>Counter</Link>
                 </li>
+                <li>
+                    <Link to='/userinfo'>UserInfo</Link>
+                </li>
             </ul>
 
             <Switch>
                 <Route exact path='/' component={Home}/>
                 <Route path='/page1' component={Page1}/>
                 <Route path='/counter' component={Counter}/>
+                <Route path='/userinfo' component={UserInfo}/>
             </Switch>
         </div>
     </Router>
