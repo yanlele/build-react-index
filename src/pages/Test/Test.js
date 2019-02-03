a5981f03bec88c72facbaa12daa3ed2aa492b991
yanle <yanle@yanledeMacBook-Pro.local>
2019-02-03 16:26:54 +0800

    add demo formik simple code

--- src/pages/Test/Test.js
+++ src/pages/Test/Test.js
@@ -1,92 +1,81 @@
 import React from 'react';
-import { Formik } from 'formik';
+import {Field, Form, Formik} from 'formik';
 
-const Basic = () => (
-    <div>
-        <h1>My Form</h1>
-        <p>This can be anywhere in your application</p>
-        {/*
-          The benefit of the render prop approach is that you have full access to React's
-          state, props, and composition model. Thus there is no need to map outer props
-          to values...you can just set the initial values, and if they depend on props / state
-          then--boom--you can directly access to props / state.
-          The render prop accepts your inner form component, which you can define separately or inline
-          totally up to you:
-          - `<Formik render={props => <form>...</form>}>`
-          - `<Formik component={InnerForm}>`
-          - `<Formik>{props => <form>...</form>}</Formik>` (identical to as render, just written differently)
-        */}
-        <Formik
-            initialValues={{
-                email: '',
-                password: '',
-            }}
-            validate={values => {
-                // same as above, but feel free to move this into a class method now.
-                let errors = {};
-                if (!values.email) {
-                    errors.email = 'Required';
-                } else if (
-                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-                ) {
-                    errors.email = 'Invalid email address';
-                }
-                return errors;
-            }}
-            onSubmit={(
-                values,
-                { setSubmitting, setErrors /* setValues and other goodies */ }
-            ) => {
-                console.log('submit: ', values);
-                /*LoginToMyApp(values).then(
-                    user => {
-                        setSubmitting(false);
-                        // do whatevs...
-                        // props.updateUser(user)
-                    },
-                    errors => {
-                        setSubmitting(false);
-                        // Maybe transform your API's errors into the same shape as Formik's
-                        setErrors(transformMyApiErrors(errors));
-                    }
-                );*/
-            }}
-            render={({
-                         values,
-                         errors,
-                         touched,
-                         handleChange,
-                         handleBlur,
-                         handleSubmit,
-                         isSubmitting,
-                     }) => {
-                console.log(touched);
-                return (
-                    <form onSubmit={handleSubmit}>
-                        <input
-                            type="email"
-                            name="email"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.email}
-                        />
-                        {touched.email && errors.email && <div>{errors.email}</div>}
-                        <input
-                            type="password"
-                            name="password"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.password}
-                        />
-                        {touched.password && errors.password && <div>{errors.password}</div>}
+const EditUserDialog = ({user, updateUser, onClose}) => {
+    return (
+        <div onClose={onClose}>
+            <h1>Edit User</h1>
+            <Formik
+                initialValues={user /** { email, social } */}
+                onSubmit={(values, actions) => {
+                    console.log(values);
+                    setTimeout(()=> actions.setSubmitting(false), 2000);
+                    /*CallMyApi(user.id, values).then(
+                        updatedUser => {
+                            actions.setSubmitting(false);
+                            updateUser(updatedUser), onClose();
+                        },
+                        error => {
+                            actions.setSubmitting(false);
+                            actions.setErrors(transformMyAPIErrorToAnObject(error));
+                        }
+                    );*/
+                }}
+                render={({errors, touched, isSubmitting}) => (
+                    <Form>
+                        <Field type="id" name="id"/>
+                        {errors.id && <div>{errors.id}</div>} <br/>
+                        <Field type="text" name="email"/>
+                        {errors.email && touched.email && <div>{errors.email}</div>} <br/>
+                        <Field type="text" name="social.facebook"/>
+                        {errors.social && errors.social.facebook &&
+                        touched.facebook && <div>{errors.social.facebook}</div>} <br/>
+                        <Field type="text" name="social.twitter"/>
+                        {errors.social && errors.social.twitter &&
+                        touched.social.twitter && <div>{errors.social.twitter}</div>} <br/>
                         <button type="submit" disabled={isSubmitting}>
                             Submit
                         </button>
-                    </form>
-                )
-            }}
-        />
-    </div>
-);
+                    </Form>
+                )}
+            />
+        </div>
+    );
+};
 
-export default Basic;
+
+class Test extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {
+                id: '',
+                email: '',
+                social: {
+                    facebook: '',
+                    twitter: '',
+                }
+            }
+        }
+    };
+
+    handleUpdateUser = (user) => {
+        this.setState({
+            user
+        })
+    };
+
+    render() {
+        console.log(this.state.user);
+        return (
+            <div>
+                {EditUserDialog({
+                    user: this.state.user,
+                    updateUser: this.handleUpdateUser
+                })}
+            </div>
+        )
+    }
+}
+
+export default Test;
