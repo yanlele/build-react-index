da3ecff34dbf2ee1cf65807a18b07a2893458d5c
yanle <yanle@yanledeMacBook-Pro.local>
2019-03-15 16:23:59 +0800

    [+]react base simple test

--- src/pages/Test/__test__/index.test.js
+++ src/pages/Test/__test__/index.test.js
@@ -3,7 +3,7 @@ import {mount} from 'enzyme';
 import Test from '../index';
 
 describe('Test', () => {
-  const checkbox = mount(<Test labelOff="off" labelOn="on"/>)
+  const checkbox = mount(<Test labelOff="off" labelOn="on"/>);
   it('default test eq off', function () {
     expect(checkbox.text()).toEqual('off');
   });
@@ -11,5 +11,10 @@ describe('Test', () => {
   it('change and eq on', function () {
     checkbox.find('input').simulate('change');
     expect(checkbox.text()).toEqual('on');
+  });
+
+  it('change times 2 and eq off', function () {
+    checkbox.find('input').simulate('change');
+    expect(checkbox.text()).toEqual('off');
   });
 });
