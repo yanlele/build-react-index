3cc3ea9b4cd5e3999162de6c1ba7d878b1debcd4
yanle <yanle@yanledeMacBook-Pro.local>
2019-03-15 20:29:06 +0800

    [+]complete Link complete test

--- src/pages/Test/__test__/Link.test.js
+++ src/pages/Test/__test__/Link.test.js
@@ -1,18 +1,19 @@
 /*快照测试*/
 import React from 'react';
-import {render} from 'enzyme';
-import toJson from 'enzyme-to-json';
+import {shallow} from 'enzyme';
+// import toJson from 'enzyme-to-json';
+import render from 'react-test-renderer';
 import ALink from '../Link';
 
 describe('Link', () => {
   it('正确的快照', function () {
-    const tree = render(<ALink page="http://www.facebook.com">facebook</ALink>);
-    expect(toJson(tree)).toMatchSnapshot();
+    const tree = render.create(<ALink page="http://www.facebook.com">facebook</ALink>).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 
   it('这个是正确的快照更改', function () {
-    const tree = render(<ALink page="http://www.yanleweb.com">yanlelele</ALink>);
-    expect(toJson(tree)).toMatchSnapshot();
+    const tree = render.create(<ALink page="http://www.yanleweb.com">yanlelele</ALink>).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 
   it('这种时候，快照就要报错了', function () {
@@ -25,5 +26,17 @@ describe('Link', () => {
       createAt: expect.any(Date),
       id: expect.any(Number),
     });
+  });
+
+  const alink = shallow(<ALink page="http://www.facebook.com">facebook</ALink>);
+  it('鼠标移入时间', function () {
+    alink.find('a').simulate('mouseEnter');
+    expect(alink.state('className')).toEqual('hovered')
+  });
+
+  it('鼠标移除, ', function () {
+    const alink = shallow(<ALink page="">facebook</ALink>);
+    alink.find('a').simulate('mouseLeave');
+    expect(alink.state('className')).toEqual('normal')
   });
 });
